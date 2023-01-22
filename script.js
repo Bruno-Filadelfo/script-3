a12bc36a2483b37737011c635aaa532d63de6a58
Bruno Filadelfo <Bruno.Filadelfo@Outlook.Com>
2023-01-21 22:19:40 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,27 @@
+/*// string 1
+
+document.getElementById('exemplo').innerHTML = "Bruno";
+
+// string 2
+ 
+document.getElementById('exemplo')
+
+//string 3
+
+document.getElementsByClassName('lista')[0].innerHTML = 'Registra-se';
+
+// string 4
+
+document.getElementsByClassName('lista')
+
+// string 5 
+
+document.getElementsByTagName('lista')
+
+// string 6 
+
+document.getElementsByName('lista')*/
+
+
+
+
